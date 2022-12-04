abb69c082eaa084d6913d92c7787200113f9baa1
cssxsh <cssxsh@gmail.com>
2022-12-04 11:15:43 +0800

    update: mirai 2.13.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.20"
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
 
-    id("net.mamoe.mirai-console") version "2.13.0-RC2"
+    id("net.mamoe.mirai-console") version "2.13.1"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -26,27 +26,24 @@ mavenCentralPublish {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-okhttp:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
-    implementation("io.ktor:ktor-client-encoding:2.1.3") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
-    implementation("com.squareup.okhttp3:okhttp:4.10.0") {
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
-        exclude(group = "org.slf4j")
-    }
     compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.3.0")
     compileOnly("xyz.cssxsh.mirai:mirai-script-plugin:1.0.2")
-    // test
     testImplementation(kotlin("test"))
-    testImplementation("org.slf4j:slf4j-simple:2.0.3")
-    testImplementation("net.mamoe:mirai-logging-slf4j:2.13.0")
+    testImplementation("org.luaj:luaj-jse:3.0.1")
+    testImplementation("org.jline:jline:3.21.0")
+    //
+    implementation(platform("net.mamoe:mirai-bom:2.13.1"))
+    compileOnly("net.mamoe:mirai-console-compiler-common")
+    testImplementation("net.mamoe:mirai-core-mock")
+    testImplementation("net.mamoe:mirai-logging-slf4j")
+    //
+    implementation(platform("io.ktor:ktor-bom:2.1.3"))
+    implementation("io.ktor:ktor-client-okhttp")
+    implementation("io.ktor:ktor-client-encoding")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    //
+    implementation(platform("org.slf4j:slf4j-parent:2.0.5"))
+    testImplementation("org.slf4j:slf4j-simple")
 }
 
 mirai {
