c83430b1527b9a27e53eae7d27cc57395f54dce9
cssxsh <cssxsh@gmail.com>
2023-07-25 21:23:28 +0800

    update: dependencies mirai 2.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.serialization") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
 
-    id("net.mamoe.mirai-console") version "2.14.0"
+    id("net.mamoe.mirai-console") version "2.15.0"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
@@ -26,7 +26,7 @@ mavenCentralPublish {
 }
 
 dependencies {
-    compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.3.3")
+    compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.4.0")
     compileOnly("xyz.cssxsh.mirai:mirai-script-plugin:1.1.0")
     testImplementation(kotlin("test"))
     testImplementation("org.luaj:luaj-jse:3.0.1")
@@ -37,11 +37,11 @@ dependencies {
     testImplementation("net.mamoe:mirai-core-mock")
     testImplementation("net.mamoe:mirai-logging-slf4j")
     //
-    implementation(platform("io.ktor:ktor-bom:2.2.1"))
+    implementation(platform("io.ktor:ktor-bom:2.2.4"))
     implementation("io.ktor:ktor-client-okhttp")
     implementation("io.ktor:ktor-client-encoding")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.6"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.7"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
